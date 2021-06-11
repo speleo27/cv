22c749df8b3ac5c99b5ebfc89fb5d5b9e7f14b10
speleo27 <seb10400@orange.fr>
2021-06-11 09:42:31 +0200

    correction d'erreur php

--- view/cv.php
+++ view/cv.php
@@ -1,14 +1,12 @@
-<?php
-echo"
 <!DOCTYPE html>
 <html lang='fr-FR'>
 
 <head>
-  <meta charset='utf-8' />
-  <link rel='stylesheet' href='../css/style.css' />
-  <link href='https://fonts.googleapis.com/icon?family=Material+Icons' rel='stylesheet'>
-  <link rel='shortcut icon' href='../images/concorde.jpg' type='image'>
-  <title>Curriculum Vitae Rossi Sébastien</title>
+    <meta charset='utf-8'/>
+    <link rel='stylesheet' href='../css/style.css'/>
+    <link href='https://fonts.googleapis.com/icon?family=Material+Icons' rel='stylesheet'>
+    <link rel='shortcut icon' href='../images/concorde.jpg' type='image'>
+    <title>Curriculum Vitae Rossi Sébastien</title>
 </head>
 
 <body>
@@ -19,52 +17,83 @@ echo"
     <p id='contact'>4 rue du four <br />
       10400 Pont sur Seine<br />
       <i class='material-icons contact-icons'> phone</i><a href='tel:0603935298'>06.03.93.52.98</a><br />
-      <i class='material-icons contact-icons'> mail_outline </i><a href='mailto:seb10400@orange.fr'>seb10400@orange.fr</a>
+        <i class='material-icons contact-icons'> mail_outline </i><a
+            href='mailto:seb10400@orange.fr'>seb10400@orange.fr</a>
     </p>
-    <h1>Sébastien Rossi</h1>
-    <p id='devise'>
-      On improvise, on s'adapte et on domine
-    </p>
-    <section>
-      <h2>Mon Expérience</h2>
-      <ul>
-        <li><strong>2019-2020</strong> Stage de 152 h chez PHEC conseil: réalisation en autonomie d'un site <a href='https://clpieddu10.fr'>vitre pour une fabrique de chaussettes</a></li><br>
-        <li><strong>2016-2019</strong> Ambulancier diplomé d\'état: transport de patient programmé et intervention à la demande du SAMU 10.SAS Garnier Romilly sur seine</li><br>
-        <li><strong>2014-2016</strong> Dirigeant de société de mécanique moto: auto entrepreneur, intervention au domicile des clients pour entretien et dépannage 2 roues motorisé. Rossi MécaMoto Vernon, Nancy</li><br>
-        <li><strong>2006-2014</strong> Sous-Officier de l\'Armée de l\'air: gestion de stocks et du personnel. Base aérienne 105,Evreux</li><br>
-        <li><strong>2004-2006</strong> Gendarme adjoint volontaire: gestion du bar de l\'école. Ecole de gendarmerie de Fontainebleau, Fontainebleau</li>
-      </ul>
-    </section>
+      <h1>Sébastien Rossi</h1>
+      <p id='devise'>
+          On improvise, on s'adapte et on domine
+      </p>
+      <section>
+          <h2>Mon Expérience</h2>
+          <ul>
+              <li><strong>2019-2020</strong> Stage de 152 h chez PHEC conseil: réalisation en autonomie d'un site <a
+                      href='https://clpieddu10.fr'>vitre pour une fabrique de chaussettes</a></li>
+              <br>
+              <li><strong>2016-2019</strong> Ambulancier diplomé d'état: transport de patient programmé et intervention
+                  à la demande du SAMU 10.SAS Garnier Romilly sur seine
+              </li>
+              <br>
+              <li><strong>2014-2016</strong> Dirigeant de société de mécanique moto: auto entrepreneur, intervention au
+                  domicile des clients pour entretien et dépannage 2 roues motorisé. Rossi MécaMoto Vernon, Nancy
+              </li>
+              <br>
+              <li><strong>2006-2014</strong> Sous-Officier de l'Armée de l'air: gestion de stocks et du personnel. Base
+                  aérienne 105,Evreux
+              </li>
+              <br>
+              <li><strong>2004-2006</strong> Gendarme adjoint volontaire: gestion du bar de l'école. Ecole de
+                  gendarmerie de Fontainebleau, Fontainebleau
+              </li>
+          </ul>
+      </section>
 
-    <section>
-      <h2>Ma Formation</h2>
-      <ul>
-         <li><strong>2021</strong> Titre Professionnel Développeur web et web mobile Greta Sud Champagne Troyes</li>
-        <li><strong>2019</strong> Certification de réussite formation Apprendre à coder en JavaScript<a href='https://openclassrooms.com'> Openclassrooms</a></li>
+      <section>
+          <h2>Ma Formation</h2>
+          <ul>
+              <li><strong>2021</strong> Titre Professionnel Développeur web et web mobile Greta Sud Champagne Troyes
+              </li>
+              <li><strong>2019</strong> Certification de réussite formation Apprendre à coder en JavaScript<a
+                      href='https://openclassrooms.com'> Openclassrooms</a></li>
 
-        <li><strong>2019</strong> Certification de réussite formation HTML5 CSS3<a href='https://openclassrooms.com'> Openclassrooms</a></li><br>
+              <li><strong>2019</strong> Certification de réussite formation HTML5 CSS3<a
+                      href='https://openclassrooms.com'> Openclassrooms</a></li>
+              <br>
 
-        <li><strong> 2017</strong> Diplôme d\'état ambulancier Aftral ifa Torvilliers</li>
+              <li><strong> 2017</strong> Diplôme d'état ambulancier Aftral ifa Torvilliers</li>
 
-        <li><strong>2016</strong> Auxiliaire ambulancier Aftral ifa Torvillier</li><br>
+              <li><strong>2016</strong> Auxiliaire ambulancier Aftral ifa Torvillier</li>
+              <br>
 
-        <li><strong>2014 </strong> Titre Professionnel Mécanicien Réparateur cycle motocycle Centre Militaire de Formation Professionnel, Fontenay-le-Comte</li><br>
+              <li><strong>2014 </strong> Titre Professionnel Mécanicien Réparateur cycle motocycle Centre Militaire de
+                  Formation Professionnel, Fontenay-le-Comte
+              </li>
+              <br>
 
-        <li><strong>2013</strong> Sauvetage au combat de niveau 1 base aérienne 105 Evreux</li>
+              <li><strong>2013</strong> Sauvetage au combat de niveau 1 base aérienne 105 Evreux</li>
 
-        <li><strong>2007</strong> Brevet élémentaire de sous-officier de l\'Armée de l'Air, spécialité restauration-hôtellerie École des fourriers de Querqueville</li>
+              <li><strong>2007</strong> Brevet élémentaire de sous-officier de l'Armée de l'Air, spécialité
+                  restauration-hôtellerie École des fourriers de Querqueville
+              </li>
 
-        <li><strong>2007</strong> Certificat d\'aptitude militaire (obtention du grade de sergent) École des sous-officiers de l\'Armée de l'Air de Rochefort</li><br>
+              <li><strong>2007</strong> Certificat d'aptitude militaire (obtention du grade de sergent) École des
+                  sous-officiers de l'Armée de l'Air de Rochefort
+              </li>
+              <br>
 
-        <li><strong>2003</strong> BTS hôtellerie restauration, option B art de la table. Lycée professionnel Jean Quarré, Paris</li>
+              <li><strong>2003</strong> BTS hôtellerie restauration, option B art de la table. Lycée professionnel Jean
+                  Quarré, Paris
+              </li>
 
-        <li><strong>2001</strong> Bac Professionnel hôtellerie restauration, mention assez bien. Lycée professionnel Jean Quarré, Paris</li>
-      </ul>
-    </section>
-    <section id='apt'>
-      <h2>Aptitude</h2>
-        <ul>
-          <li>Travail en équipe</li>
+              <li><strong>2001</strong> Bac Professionnel hôtellerie restauration, mention assez bien. Lycée
+                  professionnel Jean Quarré, Paris
+              </li>
+          </ul>
+      </section>
+      <section id='apt'>
+          <h2>Aptitude</h2>
+          <ul>
+              <li>Travail en équipe</li>
           <li>Analyse de demande</li>
           <li>Ecoute</li>
           <li>Gestion du stress</li>
@@ -83,14 +112,14 @@ echo"
 
         <h2>Divers</h2>
         <ul>
-          <li>39 ans </li>
-          <li>Reconnu en Qualité de Travailleur Handicapé</li>
-          <li>Anglais : niveau B1-2 </li>
-          <li>Espagnol: lu,écrit, parlé niveau moyen</li>
-          <li>Pratique de la guitare, simulation aérienne</li>
+            <li>39 ans</li>
+            <li>Reconnu en Qualité de Travailleur Handicapé</li>
+            <li>Anglais : niveau B1-2</li>
+            <li>Espagnol: lu,écrit, parlé niveau moyen</li>
+            <li>Pratique de la guitare, simulation aérienne</li>
         </ul>
-    </section>
+      </section>
   </div>
 </body>
 
-</html>";
+</html>
